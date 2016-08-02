dec6ec37f60c970abb08664eb93e01aac5b306ba
dogialong <longdogialong123@gmail.com>
2016-08-02 15:14:34 +0700

    commit lan dau

---
+++ app/src/main/java/com/example/vtree/calculator/Operation.java
@@ -0,0 +1,8 @@
+package com.example.vtree.calculator;
+
+/**
+ * Created by longdg123 on 8/2/2016.
+ */
+interface  Operation {
+    void perfomedMath(float a , float b);
+}
---
+++ app/src/main/java/com/example/vtree/calculator/Subtraction.java
@@ -0,0 +1,11 @@
+package com.example.vtree.calculator;
+
+/**
+ * Created by longdg123 on 8/2/2016.
+ */
+public class Subtraction implements Operation {
+    @Override
+    public void perfomedMath(float a, float b) {
+        System.out.println(a-b);
+    }
+}
